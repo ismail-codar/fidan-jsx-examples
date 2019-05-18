6ef63ef11de61a9e4a8f67f649f170b611d6076f
ismail codar <icodar@gmail.com>
2019-05-18 18:31:06 +0300

    hmr

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ const view1 = <div>Hello</div>;
 
 const appMain = document.getElementById("app");
 appMain.appendChild(view1 as any);
+
 if (module["hot"]) {
   module["hot"].dispose(function() {
     appMain.firstElementChild && appMain.removeChild(appMain.firstElementChild);
